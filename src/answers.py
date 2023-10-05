64c9484a231fd1e51debfac9d4bcb7167125d4f7
jlveig <jlveigantes@gmail.com>
2023-10-05 18:14:23 -0300

    respondido perguntas 9 13 e 14 da primeira rodada

---
+++ src/answers.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+def rd1_question_9(df):
+    return None
+
+def rd1_question_13(df):
+    return None
+
+def rd1_question_14(df):
+    return None
+
+
